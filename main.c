93923691f53c99ed278fd5310928251880b47fb3
fsaltunyuva <furkansafaaltunyuva@gmail.com>
2024-05-09 17:16:01 +0300

    Variable Location Changes - Additional Comments

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <semaphore.h>
 
-#define FILE_NAME_TEMPLATE "file%d.txt"
+#define FILE_NAME_TEMPLATE "/file%d.txt"
 
 // Function prototypes
 int isPrime(int number);
@@ -21,10 +21,13 @@ typedef struct _thstruct {
 sem_t simultaneous_thread_limit_sem;
 sem_t mutex;
 
+// Global variables
+char* file_directory_arg;
+//int read_file_count = 1;
+
 int main(int argc, char *argv[]) {
     int simultaneous_thread_count = atoi(argv[2]); // argv[2] is the number of threads
-
-    printf("Simultaneous thread count: %d\n", simultaneous_thread_count);
+    file_directory_arg = argv[1];
 
     // Counting how many files there are in the directory
     int current_file_number = 1;
@@ -44,30 +47,22 @@ int main(int argc, char *argv[]) {
     } while (file);  // Continue until fopen returns NULL (i.e. the file does not exist)
 
     int file_count = current_file_number - 1; // Subtract 1 because the last file does not exist
-
-    printf("There are %d files in the directory\n", file_count);
     // End of counting files
 
-
     sem_init(&simultaneous_thread_limit_sem, 0, simultaneous_thread_count); // Initialize the semaphore with the number of threads
-    sem_init(&mutex, 0, 1); // Not used yet ???
-
+    sem_init(&mutex, 0, 1);
     pthread_t threads[file_count];
     THREAD_ARG thread_args[file_count];
 
-    for(int i = 0; i < file_count; i++) { // Sending the file number to the thread as an argument
+    for(int i = 0; i < file_count; i++) { // Sending the file number to the thread as an argument (Using struct because there may be additional arguments in future)
         thread_args[i].file_number = i + 1;
-        thread_args[i].directory_arg = argv[1];
     }
 
-    for(int i = 0; i < file_count; i++) {
+    for(int i = 0; i < file_count; i++) { // Creating the threads
         pthread_create(&threads[i], NULL, countPrimes, &thread_args[i]);
     }
 
-    //TODO: Create threads whenever an active thread is done ???
-    //TODO: Observe the time data from terminal
-
-    for (int i = 0; i < file_count; i++) {
+    for (int i = 0; i < file_count; i++) { // Waiting for the threads to finish
         pthread_join(threads[i], NULL);
     }
 
@@ -78,6 +73,7 @@ void generate_file_path(char *buffer, size_t buffer_size, const char *directory,
     // All files are named file1.txt, file2.txt, file3.txt, etc. Therefore, I can use a template to generate the file name
     // argv[1] serves as the directory location
     snprintf(buffer, buffer_size, "%s" FILE_NAME_TEMPLATE, directory, file_number);
+    // The generated file path will be stored in the buffer (For example, myDir/file1.txt)
 }
 
 void* countPrimes(void* arg) {
@@ -93,9 +89,11 @@ void* countPrimes(void* arg) {
     int readLineCount = 0;
 
     char file_path[256];
-    generate_file_path(file_path, sizeof(file_path), thread_arg->directory_arg, thread_arg->file_number);
+    generate_file_path(file_path, sizeof(file_path), file_directory_arg, thread_arg->file_number); // Generate the file path for the current thread's file
 
-    printf("This thread is reading file %s\n", file_path);
+    //sem_wait(&mutex);
+    //generate_file_path(file_path, sizeof(file_path), thread_arg->directory_arg, read_file_count);
+    //read_file_count++;
 
     fp = fopen(file_path, "r");
 
@@ -111,18 +109,20 @@ void* countPrimes(void* arg) {
         readLineCount++;
     }
 
-    printf("This thread has found %d prime numbers\n\n", count);
+    printf("Thread %d has found %d primes in file %s\n", thread_arg->file_number, count, file_path);
 
-    // Free resources and close the file
-    if (line) {
+    //printf("Thread %d has found %d primes in file %s\n", read_file_count - 1, count, file_path);
+    //sem_post(&mutex);
+
+    if (line) { // Free resources and close the file
         free(line);
     }
-    fclose(fp);
+    fclose(fp); // Close the file pointer
 
-    sem_post(&simultaneous_thread_limit_sem);
+    sem_post(&simultaneous_thread_limit_sem); // Release the semaphore
 }
 
-int isPrime(int number)
+int isPrime(int number) // Function to check if a number is prime
 {
     if (number <= 1)
         return 0;
